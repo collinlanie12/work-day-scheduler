060a082fe36c3a796f3eb3b1dad4df74a6fc531c
Collin Lanier <collinlanier12@gmail.com>
2020-08-11 23:53:08 -0400

    trying to add local storage to calendar

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,26 @@
 $(document).ready(function () {
 
     $("#currentDay").text(moment().format('MMMM Do YYYY'));
+
     hourSet();
+
+    $("time9 .description").val(localStorage.getItem("time9"));
+    $("time9 .description").val(localStorage.getItem("time10"));
+    $("time9 .description").val(localStorage.getItem("time11"));
+    $("time9 .description").val(localStorage.getItem("time12"));
+    $("time9 .description").val(localStorage.getItem("time13"));
+    $("time9 .description").val(localStorage.getItem("time14"));
+    $("time9 .description").val(localStorage.getItem("time15"));
+    $("time9 .description").val(localStorage.getItem("time16"));
+    $("time9 .description").val(localStorage.getItem("time17"));
+    $("time9 .description").val(localStorage.getItem("time18"));
 
     function hourSet() {
         var hour = moment().hour();
-        console.log(hour);
 
         $(".time-block").each(function () {
             var timeInDay = parseInt($(this).attr("id").split("time")[1]);
-            console.log(timeInDay);
+
             if (timeInDay < hour) {
                 $(this).addClass("past");
                 $(this).removeClass("present");
@@ -25,4 +36,12 @@ $(document).ready(function () {
             }
         });
     }
+
+    $(".saveBtn").on("click", function () {
+        var userInput = $(this).siblings(".description").val();
+        var workDayTime = $(this).parent().attr("id");
+
+        localStorage.setItem(JSON.stringify(workDayTime), JSON.stringify(userInput));
+        console.log(localStorage);
+    });
 });
